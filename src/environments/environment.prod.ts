a112247281133c77bc6c96a97366178bd01b9de4
Quiabo_Vegano <155786316+Guilherme-Cardozo-688@users.noreply.github.com>
2024-12-14 02:32:33 -0300

    feat(environments): Add production environment configuration and update development environment variables.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'sua-api-key-producao',
+    authDomain: 'seu-auth-domain-producao',
+    databaseURL: 'sua-database-url-producao',
+    projectId: 'seu-project-id-producao',
+    storageBucket: 'seu-storage-bucket-producao',
+    messagingSenderId: 'seu-messaging-sender-id-producao',
+    appId: 'seu-app-id-producao'
+  }
+};
